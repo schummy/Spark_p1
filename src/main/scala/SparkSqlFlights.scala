44c7d2fed460ff80847d59266b5bbac6a039761d
“scummy” <scummy@bigmir.net>
2016-02-29 11:44:01 +0200

    SQL Spark

--- src/main/scala/SparkSqlFlights.scala
+++ src/main/scala/SparkSqlFlights.scala
@@ -6,10 +6,11 @@ import org.apache.spark.sql._
   */
 object SparkSqlFlights {
   case class Carrier(code: String, description: String)
-  case class Airport(iata:String, airport:String, city:String, state:String, country:String, lat:Double, long:Double)
+  case class Airport(iata:String, airport:String, city:String, state:String, country:String, lat:String, long:Double)
   case class Flight(year:Int/*0*/, month:Int/*1*/, day:Int/*2*/,carrierCode:String/*8*/, origin:String/*16*/, dest:String/*17*/)
-def main(args:Array[String]){
-  System.setProperty("hadoop.home.dir", "C:\\")
+  val dataDir = "/Users/user/bigData/EPAM_hadoop_training/Hive_Basics_p1/airports.carrier.dataset/"
+  def main(args:Array[String]){
+   // System.setProperty("hadoop.home.dir", "C:\\")
 
   val conf = new SparkConf().setAppName("SparkSqlFlights").setMaster("local")
   val sc = new SparkContext(conf)
@@ -21,30 +22,66 @@ def main(args:Array[String]){
 
   // Create an RDD of Person objects and...
  // val people = sc.textFile()
-  sc.textFile("D:\\Share\\Hive_Basics_p1\\airports.carrier.dataset\\carriers.csv")
+  sc.textFile(dataDir + "carriers.csv")
     .mapPartitionsWithIndex { (idx, iter) => if (idx == 0) iter.drop(1) else iter }
     .map(_.split(","))
-    .map(p => Carrier(p(0), p(1)))
+    .map(p => Carrier(
+      p(0).drop(1).dropRight(1),
+      p(1).drop(1).dropRight(1)))
     .toDF.registerTempTable("carriers")
 
 
-  sc.textFile("D:\\Share\\Hive_Basics_p1\\airports.carrier.dataset\\airports.csv")
+  sc.textFile(dataDir + "airports.csv")
     .mapPartitionsWithIndex { (idx, iter) => if (idx == 0) iter.drop(1) else iter }
     .map(_.split(","))
-    .map(p => Airport(p(0), p(1), p(2), p(3), p(4), p(5).toDouble, p(6).toDouble))
+    .map(p => Airport(
+      p(0).drop(1).dropRight(1),
+      p(1).drop(1).dropRight(1),
+      p(2).drop(1).dropRight(1),
+      p(3).drop(1).dropRight(1),
+      p(4).drop(1).dropRight(1),
+      p(5),
+      p(6).toDouble))
     .toDF.registerTempTable("airports")
 
-  sc.textFile("D:\\Share\\Hive_Basics_p1\\airports.carrier.dataset\\2007.csv")
-    .cache()
+    val df = sql.read.format("com.databricks.spark.csv")
+      .option("header","true")
+      .load(dataDir + "airports.csv").collect()
+      .foreach(println)
+
+  sc.textFile(dataDir + "2007.csv")
+    //.cache()
     .mapPartitionsWithIndex { (idx, iter) => if (idx == 0) iter.drop(1) else iter }
     .map(_.split(","))
-    .map(p => Flight(p(0).toInt, p(1).toInt, p(2).toInt, p(8), p(16), p(17)))
+    .map(p => Flight(
+      p(0).toInt,
+      p(1).toInt,
+      p(2).toInt,
+      p(8),
+      p(16),
+      p(17)))
     .toDF.registerTempTable("flights")
 
-//  sql.sql("select f.carrierCode, count(*) from flights f join carriers c on c.code=f.carrierCode group by f.carrierCode")
-  sql.sql("select distinct  carrierCode from flights")
-    .collect()
-    .foreach(println)
+ /* sql.sql("select c.description, count(*) as numberOfFlights " +
+    "from flights f " +
+    "join carriers c on c.code=f.carrierCode " +
+    "group by c.description " +
+    "order by numberOfFlights desc")*/
+    sql.sql("select c.description, f.numberOfFlights " +
+      "from (select carrierCode, count(*) as numberOfFlights from flights group by carrierCode ) f " +
+      "join carriers c on c.code=f.carrierCode " +
+      "order by numberOfFlights desc")
+  //  sql.sql("select f.carrierCode, count(*) from flights f group by f.carrierCode"
+    // sql.sql("select distinct carrierCode from flights")
+   // sql.sql("select distinct code from carriers")
 
+    //  .collect()
+    //.foreach(println)
+
+
+       sql.sql("select * from airports where lat='\"USA\"'")
+
+      .collect()
+      .foreach(println)
 }
 }
