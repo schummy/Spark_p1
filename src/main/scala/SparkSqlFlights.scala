65649df6656f1225e9075812ab4d89b177b0bc64
Andrii_Krasnolob <schummy@bigmir.net>
2016-03-01 14:13:36 +0200

    Spark HQL - stable

--- src/main/scala/SparkSqlFlights.scala
+++ src/main/scala/SparkSqlFlights.scala
@@ -16,7 +16,7 @@ object SparkSqlFlights {
     if ( System.getProperty("os.name") == "Windows 7") {
         System.setProperty("hadoop.home.dir", "C:\\BigData\\Hadoop")
         dataDir = "D:\\Share\\Hive_Basics_p1\\airports.carrier.dataset\\"
-    }else{
+    } else {
         dataDir = "/Users/user/bigData/EPAM_hadoop_training/Hive_Basics_p1/airports.carrier.dataset/"
     }
     println(dataDir)
@@ -24,29 +24,44 @@ object SparkSqlFlights {
   val conf = new SparkConf().setAppName("SparkSqlFlights").setMaster("local")
 
   val sc = new SparkContext(conf)
-  //val sql = new SQLContext(sc)
   val hql = new HiveContext(sc)
   import hql.implicits._
 
   // Define the schema using a case class.
 
 
-  val carrierRegExp = "\"(.*)\",\"(.*)\""r
+/*  val carrierRegExp = "\"(.*)\",\"(.*)\""r
 
   sc.textFile(dataDir + "carriers.csv")
     .mapPartitionsWithIndex { (idx, iter) => if (idx == 0) iter.drop(1) else iter }
     .map(line => { var carrierRegExp(code, description) = line
       new Carrier(code, description) })
-   // .toDF.registerTempTable("carriers")
+    .toDF.registerTempTable("carriers")*/
+
+    hql.read
+      .format("com.databricks.spark.csv")
+      .option("header", "true") // Use first line of all files as header
+      .option("inferSchema", "true") // Automatically infer data types
+      .option("escape", "\\")
+      .load(dataDir + "carriers.csv")
+      .toDF().registerTempTable("carriers")
 
 
-  val airportsRegExp = "\"(.*)\",\"(.*)\",\"?(.*[^\"])\"?,\"?(.*[^\"])\"?,\"(.*[^\"])\",([0-9.-]+),([0-9.-]+)"r
+/*  val airportsRegExp = "\"(.*)\",\"(.*)\",\"?(.*[^\"])\"?,\"?(.*[^\"])\"?,\"(.*[^\"])\",([0-9.-]+),([0-9.-]+)"r
 
   sc.textFile(dataDir + "airports.csv")
     .mapPartitionsWithIndex { (idx, iter) => if (idx == 0) iter.drop(1) else iter }
     .map(line => { var airportsRegExp(iata,airport,city,state,country,lat,long) = line
       new Airport(iata,airport,city,state,country,lat.toDouble,long.toDouble) })
-   // .toDF.registerTempTable("airports")
+    .toDF.registerTempTable("airports")*/
+
+    hql.read
+      .format("com.databricks.spark.csv")
+      .option("header", "true") // Use first line of all files as header
+      .option("inferSchema", "true") // Automatically infer data types
+      .option("escape", "\\")
+      .load(dataDir + "airports.csv")
+      .toDF().registerTempTable("airports")
 
   sc.textFile(dataDir + "2007.csv")
     .mapPartitionsWithIndex { (idx, iter) => if (idx == 0) iter.drop(1) else iter }
@@ -61,24 +76,36 @@ object SparkSqlFlights {
      Array(p(16), p(17)))
     )
     .toDF.registerTempTable("flights")
-   /* sql.cacheTable("flights")
-    sql.cacheTable("airports")*/
 
-/* task 6
-    sql.sql("select c.description, f.numberOfFlights " +
+
+    /*hql.cacheTable("flights")
+    hql.cacheTable("airports")*/
+
+// task 6
+/*    hql.sql("select c.description, f.numberOfFlights " +
       "from (select carrierCode, count(*) as numberOfFlights from flights group by carrierCode ) f " +
       "join carriers c on c.code=f.carrierCode " +
       "order by numberOfFlights desc")
       .collect()
-     .foreach(println)
-*/
+     .foreach(println)*/
 
 
 
-/* task 7
-    sql.sql("select iata from airports where city=\"New York\"")
+// task 7
+/*
+    hql.sql("select iata from airports where city=\"New York\"")
       .registerTempTable("NYC_airports")
-    sql.sql("select sum(number) from (select count(*) number from flights f join NYC_airports a on a.iata=f.origin " +
+
+  hql.sql("SELECT count(*) from (" +
+    "SELECT airport FROM flights " +
+    "LATERAL VIEW explode(airports) tmpTable AS airport " +
+    "WHERE year=2007 and month=6" +
+    ") f " +
+    "JOIN NYC_airports a on a.iata=f.airport")
+    .collect()
+    .foreach(println)
+*/
+/*    hql.sql("select sum(number) from (select count(*) number from flights f join NYC_airports a on a.iata=f.origin " +
       "where year=2007 and month=6 " +
       "union " +
       "select count(*) number from flights f join NYC_airports a on a.iata=f.dest " +
@@ -88,20 +115,16 @@ object SparkSqlFlights {
 */
 
 // task 8
-/*    sql.sql("select distinct iata, airport from airports where country=\"USA\"")
-      .registerTempTable("USA_airports")*/
+/*    hql.sql("select distinct iata, airport from airports where country=\"USA\"")
+      .registerTempTable("USA_airports")
 
-/*    sql.sql(
-  "select dest as iata, count(*) number from flights  join USA_airports a on dest=a.iata " +
-  "where month between 6 and 8 " +
-  "group by dest " +
-    "order by number desc " +
-    "limit 5"
-)
-  .collect()
-  .foreach(println)*/
+    hql.sql("select a.airport from (SELECT airport, count(*) number FROM flights LATERAL VIEW explode(airports) tmpTable AS airport " +
+      "where month between 6 and 8 " +
+      "group by airport order by number desc limit 5 ) tmp join USA_airports a on a.iata=tmp.airport")
+      .collect()
+      .foreach(println)*/
 
-/*    sql.sql(
+/*    hql.sql(
     "select airport from " +
     "( select a.iata, count(*) flightsCount from (" +
         "select dest as iata from flights " +
@@ -114,11 +137,9 @@ object SparkSqlFlights {
     "order by flightsCount desc " +
     "limit 5 )top5 join airports a on top5.iata=a.iata"
 )*/
-      hql.sql("SELECT airport, count(*) number FROM flights LATERAL VIEW explode(airports) tmpTable AS airport group by airport order by number desc")
-  .collect()
-  .foreach(println)
 
-/*    sql.sql(
+
+/*    hql.sql(
   "select tmp.iata, sum(number) flightsCount from (" +
   "select dest as iata, count(*) number from flights " +
   "where month between 6 and 8 " +
@@ -136,13 +157,13 @@ object SparkSqlFlights {
 
 
 
-/* task 9
-  sql.sql("select description from (select carrierCode, count(*) numberOfLights from flights group by carrierCode order by numberOfLights limit 1) a " +
+// task 9
+/*
+  hql.sql("select description from (select carrierCode, count(*) numberOfLights from flights group by carrierCode order by numberOfLights limit 1) a " +
     "join carriers on carrierCode=code")
   .collect()
   .foreach(println)
 */
 
-  /*  sql.sql("select count(*) from flights").collect().foreach(println)*/
 }
 }
