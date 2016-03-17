0fac118b748f871e0abae7f47b55825d62906ef7
Andrii_Krasnolob <schummy@bigmir.net>
2016-03-17 17:02:49 +0200

    SparkStream to Kafka + Hive

--- src/main/scala/SparkStreamSandbox.scala
+++ src/main/scala/SparkStreamSandbox.scala
@@ -4,7 +4,8 @@ import java.util.Properties
 import org.apache.kafka.clients.producer.{KafkaProducer, ProducerConfig}
 import org.apache.log4j.{Level, LogManager}
 import org.apache.spark._
-import org.apache.spark.sql.{Row, DataFrame}
+import org.apache.spark.sql.types._
+import org.apache.spark.sql.{SQLContext, Row, DataFrame}
 import org.apache.spark.sql.hive.HiveContext
 import org.apache.spark.streaming._
 import org.apache.spark.streaming.StreamingContext._
@@ -19,21 +20,11 @@ import scala.collection.mutable
 object SparkStreamSandbox {
   val networkDeviceName: String = "\\Device\\NPF_{6AE37950-B19A-4A4F-B3AC-2425236BE527}"
   val checkpointDirectory: String = "D:\\Share\\Spark_Basics_p2\\checkpoint"
+  private val windowWidth: Duration = Seconds(10)
   val numberOfThreads = 3
   val batchInterval: Long = 1
 
   def main(args:Array[String]) {
-/*
-    val setTmp = new IPSettings()
-    setTmp.add("255.255.255.255", 1, 10.0, 500L)
-    setTmp.add("255.255.255.255", 2, 20.0, 500L)
-    setTmp.add(null, 2, 30.0, 500L)
-    setTmp.add(null, 1, 40.0, 400L)
-    println(setTmp)
-
-    println (setTmp.get("255.255.285.255"))
-*/
-
     if ( System.getProperty("os.name") == "Windows 7") {
       System.setProperty("hadoop.home.dir", "C:\\BigData\\Hadoop")
     }
@@ -43,34 +34,54 @@ object SparkStreamSandbox {
     ssc.awaitTermination()  // Wait for the computation to terminate
   }
 
+  def createContext(): StreamingContext = {
+    val conf = new SparkConf().setMaster(s"local[$numberOfThreads]").setAppName(SparkStreamSandbox.getClass.toString)
+    val ssc = new StreamingContext(conf, Seconds(batchInterval))
+    ssc.checkpoint(checkpointDirectory)
+    processData(ssc)
+    ssc.remember(Seconds(10))
+    ssc
+  }
+
   def processData(ssc: StreamingContext): Unit = {
     val lines = ssc.receiverStream(new CustomReceiverJnetPcap(networkDeviceName))
-    val hql = new HiveContext(ssc.sparkContext)
-    val settings = loadSettings(hql).collect
     val ipSettings = new IPSettings
+    HiveContextSingleton.setSparkCOntext(ssc.sparkContext)
+    var hql = HiveContextSingleton.hql
+    createHourlyStatisticTable(hql)
 
-    settings.foreach(r => ipSettings.add(
-      r.getAs[String]("hostIp")
-      , r.getAs[Long]("limitType").toInt
-      , r.getAs[Double]("value")
-      , r.getAs[Long]("period")))
+    val settings = loadSettings(hql).collect.
+      foreach(r => ipSettings.add(
+        r.getAs[String]("hostIp")
+        , r.getAs[Long]("limitType").toInt
+        , r.getAs[Double]("value")
+        , r.getAs[Long]("period")))
 
     val reducedStream = lines
       .reduceByKey((a, b) => (a._1 + b._1, a._2 + b._2))
 
-    val hourlySnaphot = reducedStream.window(Minutes(1), Minutes(1)).reduceByKeyAndWindow( (a, b) => (a._1 + b._1, a._2 + b._2), Minutes(1) )
-
-
-    hourlySnaphot
-      .foreachRDD(
-        (rdd,time) => {
-          hql.createDataFrame(rdd).registerTempTable("temp")
-          hql.sql("select *, '"+time+"' as timeStamp from temp").collect().foreach(println)
-          //hql.sql("INSERT INTO hourlySatistic select * from temp")
-        }
-
+    val schema =
+      StructType(
+        (Array(StructField("HostIP", StringType, true)
+          , StructField("TrafficConsumed", IntegerType, true)
+          , StructField("AverageSpeed", DoubleType, true))
+          )
       )
 
+    val hourlySnaphot = reducedStream.window(windowWidth, windowWidth)
+      .reduceByKeyAndWindow( (a, b) => (a._1 + b._1, a._2 + b._2), windowWidth )
+      .map(a => Row(a._1, a._2._1,  a._2._1.toDouble/a._2._2))
+
+    hourlySnaphot.foreachRDD(
+        (rdd,time) => {
+          HiveContextSingleton.setSparkCOntext(rdd.sparkContext)
+          var hql = HiveContextSingleton.hql
+          hql
+            .createDataFrame(rdd, schema)
+            .registerTempTable("temp")
+          hql.sql("INSERT INTO table hourlySatistic select  '"+time.milliseconds.toString+"', *  from temp")
+        }
+      )
 
     val packetsWithSettings=reducedStream
       .map(r => (r._1, (new IPPacketsInfo(r._2._1, r._2._2), ipSettings.get(r._1))))
@@ -98,56 +109,6 @@ object SparkStreamSandbox {
         )
       }
       )
-    //wordCountStateStream.print()
-
-    /*
-        val stateSnapshotStream = wordCountStateStream.stateSnapshots()
-
-
-        stateSnapshotStream.foreachRDD((rdd, time) => {
-          println("-" * 40 + time)
-
-          //kafkaSink.send("alerts", "-" * 40 + time)
-
-          rdd.foreachPartition(
-            p => p.foreach(x => {
-
-              if (x._2._1.isReadyForALert) {
-                println(getAlertMessage(x._1, x._2._1))
-                kafkaSink.send("alerts", getAlertMessage(x._1, x._2._1))
-              }
-              if (x._2._2.isReadyForALert) {
-                println(getAlertMessage(x._1, x._2._2))
-                kafkaSink.send("alerts", getAlertMessage(x._1, x._2._2))
-              }
-
-            }
-            )
-          )
-        }
-        )
-    */
-  }
-
-
-  def initKafkaProperties(): Properties = {
-    val props = new Properties()
-    props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092")
-    props.put(ProducerConfig.CLIENT_ID_CONFIG, "KafkaProducer")
-    props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,
-      "org.apache.kafka.common.serialization.StringSerializer")
-    props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG,
-      "org.apache.kafka.common.serialization.StringSerializer")
-    props
-  }
-
-  def createContext(): StreamingContext = {
-    val conf = new SparkConf().setMaster(s"local[$numberOfThreads]").setAppName(SparkStreamSandbox.getClass.toString)
-    val ssc = new StreamingContext(conf, Seconds(batchInterval))
-    ssc.checkpoint(checkpointDirectory)
-    processData(ssc)
-   // ssc.remember(Seconds(10))
-    ssc
   }
 
   def loadSettings(hql: HiveContext): DataFrame = {
@@ -205,4 +166,35 @@ object SparkStreamSandbox {
 
     s"${java.util.UUID.randomUUID()} ${limitType}${isBreached} for $key ${state.q.last.timeStamp} FactValue=${thresholdValue}, ThresholdValue=${state.value}, Period = ${state.period}"
   }
+
+  def createHourlyStatisticTable(hql: HiveContext): DataFrame = {
+    hql.sql("create table if not exists hourlySatistic ( " +
+      "timeStamp STRING, " +
+      "HostIP STRING, " +
+      "TrafficConsumed INT, " +
+      "AverageSpeed DOUBLE) " +
+      "STORED AS ORC")
+  }
+
+  def initKafkaProperties(): Properties = {
+    val props = new Properties()
+    props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092")
+    props.put(ProducerConfig.CLIENT_ID_CONFIG, "KafkaProducer")
+    props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,
+      "org.apache.kafka.common.serialization.StringSerializer")
+    props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG,
+      "org.apache.kafka.common.serialization.StringSerializer")
+    props
+  }
 }
+
+/*
+    val setTmp = new IPSettings()
+    setTmp.add("255.255.255.255", 1, 10.0, 500L)
+    setTmp.add("255.255.255.255", 2, 20.0, 500L)
+    setTmp.add(null, 2, 30.0, 500L)
+    setTmp.add(null, 1, 40.0, 400L)
+    println(setTmp)
+
+    println (setTmp.get("255.255.285.255"))
+*/
